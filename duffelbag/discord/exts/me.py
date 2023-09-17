09cb3568fa82c686dfcafeb6fcd638b25d078f08
Enoki <enokiun@gmail.com>
2023-09-18 00:04:20 +0300

    feat: improve the "me" command

--- duffelbag/discord/exts/me.py
+++ duffelbag/discord/exts/me.py
@@ -9,15 +9,25 @@ from duffelbag import auth
 
 plugin = plugins.Plugin()
 
+
 @plugin.slash_command()
-async def me(inter: disnake.CommandInteraction) -> None:
+async def me(inter: disnake.CommandInteraction, *, private: bool = True) -> None:
     """Get your Arknights account's general information."""
     join = database.ArknightsUser.user.join_on(database.PlatformUser.user)
-    user = await database.ArknightsUser.objects().where((join.platform_id == inter.author.id) & (join.platform_name == auth.Platform.DISCORD)).first()
+    user = (
+        await database.ArknightsUser.objects()
+        .where(
+            (join.platform_id == inter.author.id) & (join.platform_name == auth.Platform.DISCORD),
+        )
+        .first()
+    )
     if not user:
-        await inter.send("Cannot find a user, ensure that you've made a user and linked them to your arknights account")
+        await inter.send(
+            "Cannot find a user, ensure that you've made a user and linked"
+            " them to your arknights account",
+        )
         return
-    await inter.response.defer()
+    await inter.response.defer(ephemeral=private)
     client_auth = YostarAuth()
     await client_auth.login_with_token(user.channel_uid, user.yostar_token)
     client = Client(client_auth, assets=False)
@@ -26,12 +36,28 @@ async def me(inter: disnake.CommandInteraction) -> None:
     for operator in data.social.assist_char_list:
         assert operator
         troop_data = data.troop.chars[operator.char_inst_id]
-        operator_data = await database.Character.objects().where(database.Character.id == troop_data.char_id).first()
+        operator_data = (
+            await database.Character.objects()
+            .where(database.Character.id == troop_data.char_id)
+            .first()
+        )
         assert operator_data
         skill = troop_data.skills[operator.skill_index]
-        skill_level = f"m{skill.specialize_level}" if skill.specialize_level > 0 else f"l{troop_data.main_skill_lvl}"
-        operators.append(f"- {operator_data.name} e{troop_data.evolve_phase}{troop_data.level} s{operator.skill_index}l{skill_level}")
+        skill_level = (
+            f"m{skill.specialize_level}"
+            if skill.specialize_level > 0
+            else f"l{troop_data.main_skill_lvl}"
+        )
+        operators.append(
+            f"> - {operator_data.name} e{troop_data.evolve_phase}{troop_data.level}"
+            f" s{operator.skill_index}l{skill_level}",
+        )
     operators = "\n".join(operators)
-    await inter.send(f"{data.status.nickname} lvl{data.status.level}\nSupport operators:\n{operators}")
+    await inter.send(
+        f"{data.status.nickname} lvl{data.status.level}\n*{data.status.resume}*"
+        f"\n\n> Support operators:\n{operators}",
+        ephemeral=private,
+    )
+
 
 setup, teardown = plugin.create_extension_handlers()
